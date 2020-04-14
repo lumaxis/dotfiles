0966f1f2e34586feba5929fad7303339eac27793
Lukas Spieß <lukas.spiess@gmail.com>
2020-04-14 15:20:51 +0200

    Update setting of computer name

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,5 +1,8 @@
 osascript -e 'tell application "System Preferences" to quit'
 
+echo "What's the name of this computer?"
+read COMPUTER_NAME
+
 # Ask for the administrator password upfront
 sudo -v
 
@@ -11,12 +14,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-echo "What's the name of this computer?"
-read COMPUTER_NAME
-sudo scutil --set ComputerName "'$COMPUTER_NAME'"
-sudo scutil --set HostName "'$COMPUTER_NAME'"
-sudo scutil --set LocalHostName "'$COMPUTER_NAME'"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "'$COMPUTER_NAME'"
+#sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+sudo scutil --set ComputerName "$COMPUTER_NAME"
 
 # Automatically switch appearance between Light and Dark
 defaults write NSGlobalDomain AppleInterfaceStyleSwitchesAutomatically -bool true
